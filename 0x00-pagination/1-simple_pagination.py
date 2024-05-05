4747f58d2319d2831d5c37124cbee4e1fa64551a
Matrix30 <Matrixboy30@gmail.com>
2024-05-05 11:25:30 +0100

    finish task 1. Simple pagination

---
+++ 0x00-pagination/1-simple_pagination.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+"""
+contains helper function for pagination
+and Server class
+"""
+
+
+import csv
+import math
+from typing import List
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple:
+    """
+    Helper function to help in pagination
+    Args:
+                page(int): page number
+                page_size: The size of the page
+        Returns:
+                Tuple(int,int): returns a tuple of size 2 containing the
+                                start and stop index corresponding to the range
+                                of indexes
+    """
+    stop_index = 0
+    for i in range(page):
+        start_index = stop_index
+        stop_index += page_size
+
+    return (start_index, stop_index)
+
+
+class Server:
+    """Server class to paginate a database of popular baby names."""
+
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset"""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+        self.dataset()
+        if self.__dataset is None:
+            return []
+        if page_size > len(self.__dataset):
+            return []
+        start, stop = index_range(page, page_size)
+        return self.__dataset[start:stop]
