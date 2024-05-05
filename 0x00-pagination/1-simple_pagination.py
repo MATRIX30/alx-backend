3f130d2ea795e30a41197aae9c10fe7c825545fe
Matrix30 <Matrixboy30@gmail.com>
2024-05-05 17:32:21 +0100

    finish task 1. Simple pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -1,22 +1,26 @@
 #!/usr/bin/env python3
 """
-Adds `get_page` method to `Server` class
+contains helper function for pagination
+and Server class
 """
+
+
 import csv
-from typing import List, Tuple
+import math
+from typing import List
+from typing import Tuple
 
 
 class Server:
-    """Server class to paginate a database of popular baby names.
-    """
+    """Server class to paginate a database of popular baby names."""
+
     DATA_FILE = "Popular_Baby_Names.csv"
 
     def __init__(self):
         self.__dataset = None
 
     def dataset(self) -> List[List]:
-        """Cached dataset
-        """
+        """Cached dataset"""
         if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
@@ -27,22 +31,29 @@ class Server:
 
     @staticmethod
     def index_range(page: int, page_size: int) -> Tuple[int, int]:
-        """Calculate start and end index range for a `page`, with `page_size`
         """
-        nextPageStartIndex = page * page_size
-        return nextPageStartIndex - page_size, nextPageStartIndex
+        Helper function to help in pagination
+        Args:
+                    page(int): page number
+                    page_size: The size of the page
+            Returns:
+                    Tuple(int,int): returns a tuple of size 2 containing the
+                    start and stop index corresponding to the range
+                                    of indexes
+                    List([]): empty list if page size is out of range
+        """
+        stop_index = 0
+        for i in range(page):
+            start_index = stop_index
+            stop_index += page_size
+
+        return (start_index, stop_index)
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """
-        Get items for the given page number
-        Args:
-            page (int): page number
-            page_size (int): number of items per page
-        Returns:
-            (List[List]): a list of list(row) if inputs are within range
-            ([]) : an empty list if page and page_size are out of range
-        """
-        assert type(page) == int and type(page_size) == int
-        assert page > 0 and page_size > 0
-        startIndex, endIndex = self.index_range(page, page_size)
-        return self.dataset()[startIndex:endIndex]
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+        self.dataset()
+        if page_size > len(self.__dataset):
+            return []
+        start, stop = Server.index_range(page, page_size)
+        return self.dataset()[start:stop]
