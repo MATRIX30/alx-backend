b28fc1a4515759a6a37763adc80af31780c04af7
Matrix30 <Matrixboy30@gmail.com>
2024-05-05 11:36:05 +0100

    finish task 1. Simple pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -11,25 +11,6 @@ from typing import List
 from typing import Tuple
 
 
-def index_range(page: int, page_size: int) -> Tuple:
-    """
-    Helper function to help in pagination
-    Args:
-                page(int): page number
-                page_size: The size of the page
-        Returns:
-                Tuple(int,int): returns a tuple of size 2 containing the
-                                start and stop index corresponding to the range
-                                of indexes
-    """
-    stop_index = 0
-    for i in range(page):
-        start_index = stop_index
-        stop_index += page_size
-
-    return (start_index, stop_index)
-
-
 class Server:
     """Server class to paginate a database of popular baby names."""
 
@@ -47,6 +28,25 @@ class Server:
             self.__dataset = dataset[1:]
 
         return self.__dataset
+    
+    @staticmethod
+    def index_range(page: int, page_size: int) -> Tuple:
+        """
+        Helper function to help in pagination
+        Args:
+                    page(int): page number
+                    page_size: The size of the page
+            Returns:
+                    Tuple(int,int): returns a tuple of size 2 containing the
+                                    start and stop index corresponding to the range
+                                    of indexes
+        """
+        stop_index = 0
+        for i in range(page):
+            start_index = stop_index
+            stop_index += page_size
+
+        return (start_index, stop_index)
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         assert isinstance(page, int) and page > 0
@@ -54,5 +54,5 @@ class Server:
         self.dataset()
         if page_size > len(self.__dataset):
             return []
-        start, stop = index_range(page, page_size)
+        start, stop = self.index_range(page, page_size)
         return self.__dataset[start:stop]
